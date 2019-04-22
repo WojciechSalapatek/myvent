bbbbd49b70f0f4184bcd8f7b119665ab8683cd88
WojciechSalapatek <wojciech.salapatek@gmail.com>
2019-04-22 18:33:39 +0200

    Initializes project, sets up db

---
+++ src/main/java/com/wpate/myvent/MyventApplication.java
@@ -0,0 +1,13 @@
+package com.wpate.myvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyventApplication.class, args);
+    }
+
+}
